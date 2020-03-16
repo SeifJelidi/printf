aa8017c403a1dc552801e03a15e791f5c8524413
Seif Jelidi <seifjelidi3@gmail.com>
2020-03-16 13:19:42 +0000

    task 0 printf

---
+++ _printf.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include "holberton.h"
+
+/**
+ * case_string - returns length of a string
+ * Description: returns length of a string
+ * @ch: character string
+ * Return: length of a string
+ */
+
+int case_string(char *ch)
+{
+	int i;
+	int s = 0;
+
+	for (i = 0 ; ch[i] != '\0' ; i++)
+	{
+		_putchar(ch[i]);
+		s++;
+	}
+	return (s);
+}
+
+/**
+ * _printf - produces output according to a format
+ * Description: produces output according to a format
+ * @format: character string
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list list;
+	int i, s = 0;
+	char *ch;
+
+	va_start(list, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (0);
+	for (i = 0 ; format[i] != '\0' ; i++)
+	{
+		if (format[i] == '%' && format[i + 1] != '%')
+		{
+			switch (format[i])
+			{
+				case 'c':
+					_putchar(va_arg(list, int));
+					s++;
+					break;
+				case 's':
+					ch = va_arg(list, char*);
+					s += case_string(ch);
+					break;
+				default:
+					_putchar(format[i]);
+					s++;
+			}
+
+		}
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			_putchar('%');
+			s++;
+		}
+		else
+		{
+			_putchar(format[i]);
+			s++;
+		}
+	}
+	va_end(list);
+	return (s);
+}
