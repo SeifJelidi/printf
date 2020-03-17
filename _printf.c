c99c3c3fe4cbfa64f70293dbe665b0e04acc9603
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-03-17 20:42:07 +0000

    commit

--- _printf.c
+++ _printf.c
@@ -35,9 +35,6 @@ int _printf(const char *format, ...)
 				case 'b':
 					s += case_binary(va_arg(list, unsigned int));
 					break;
-				case 'r':
-					s += print_rev(va_arg(list, char *));
-					break;
 				default:
 					_putchar(format[i]), _putchar(format[i + 1]), s++;
 			}
